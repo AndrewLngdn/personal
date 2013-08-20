d4eff70e0b1a9c32d37fc741a0290506562db58f
Andrew Langdon <andrewlngdn@gmail.com>
2013-08-19 20:41:43 -0500

    added dots.html

---
+++ app/assets/javascripts/dots.js
@@ -0,0 +1,16 @@
+var Dots = {
+    init: function(){
+        var count = 0;
+        var height = window.innerHeight;
+        var width = window.innerWidth;
+        var limit = width*height/(65*65);
+        var interval = 100;
+        var a = setInterval(function(){
+            $('body').append('<div class="dot"></div>');
+            count++;
+            if (count > limit){
+                clearInterval(a);
+            }
+        }, interval);
+    }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Personal::Application.routes.draw do
   #resources :examples
   get 'kaleidosine' => 'static_pages#kaleidosine'
   get 'saturate' => 'static_pages#saturate'
+  get 'dots' => 'static_pages#dots'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
