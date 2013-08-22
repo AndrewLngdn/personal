455a86c69ac699d8a859315cf3ae0b4c9be562a1
Andrew Langdon <andrewlngdn@gmail.com>
2013-08-21 20:57:58 -0500

    started dots.js refactor

--- app/assets/javascripts/dots.js
+++ app/assets/javascripts/dots.js
@@ -1,15 +1,19 @@
 var Dots = {
+    count: 0,
+    dotHeight:0,
+    dotWidth:0,
+    interval: 100,
     init: function(){
         var count = 0;
         var height = window.innerHeight;
         var width = window.innerWidth;
         var limit = width*height/(65*65);
         var interval = 100;
-        var a = setInterval(function(){
+        var append = setInterval(function(){
             $('.container').append('<div class="dot"></div>');
             count++;
             if (count > limit){
-                clearInterval(a);
+                clearInterval(append);
             }
         }, interval);
     }
