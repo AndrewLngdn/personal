d4d9db5aa0aa783565dafda417bc327b27b78236
Andrew Langdon <andrewlngdn@gmail.com>
2013-08-21 19:13:59 -0500

    updated synth

--- app/assets/javascripts/synth.js
+++ app/assets/javascripts/synth.js
@@ -5,18 +5,20 @@ Synth = function(audioContext){
 
     this.portamento = 0.2;
 
+    this.filterFrequency = 200;
+
     this.oscillator = this.audioContext.createOscillator();
-
     this.oscillator.noteOn(0);
-
     this.oscillator.type = "sawtooth";
 
     this.gainNode = this.audioContext.createGainNode();
-
     this.gainNode.gain.value = 0;
 
-    this.oscillator.connect(this.gainNode);
+    this.filter = this.audioContext.createBiquadFilter();
+    this.filter.type = 0;
 
+    this.oscillator.connect(this.filter);
+    this.filter.connect(this.gain);
     this.gainNode.connect(this.audioContext.destination);
 }
 
@@ -24,21 +26,39 @@ Synth.prototype.noteOn = function(note){
 
     var frequency = this.notes[note] || 0;
     var now = this.audioContext.currentTime;
+    this.frequency = frequency;
 
     this.oscillator.frequency.setValueAtTime(frequency, now);
-
+    this.filter.frequency.setValueAtTime(this.filterFrequency, now);
     this.gainNode.gain.setValueAtTime(1, now);
 }
 
 Synth.prototype.noteSlide = function(note){
     var frequency = this.notes[note] || 0;
     var now = this.audioContext.currentTime;
+    this.frequency = frequency;
 
     this.oscillator.frequency.linearRampToValueAtTime(frequency, now + this.portamento);
+    this.filter.frequency.setValueAtTime(this.filterFrequency, now);
+
 }
 
 Synth.prototype.noteOff = function(){
     var now = this.audioContext.currentTime;
 
     this.gainNode.gain.setValueAtTime(0, now);
+}
+
+Synth.prototype.setFilterFrequency = function(value){
+    var now = this.audioContext.currentTime;
+    var minValue = 40;
+    var maxValue = this.audioContext.sampleRate / 2;
+    var numberOfOctaves = Math.log(maxValue / minValue) / Math.LN2;
+    var multiplier = Math.pow(2, numberOfOctaves * (value - 1.0));
+    this.filter.frequency.setValueAtTime(maxValye * multiplier, now);
+}
+
+Synth.prototype.setFilterResonance = function(value){
+    var now = this.audioContext.currentTime;
+    this.filter.Q.setValueAtTime(value * 30, now);
 }
